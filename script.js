6313fcec0b6acb77eb4354030054189e1536c759
capankaa <ahmadnazar2016@gmail.com>
2026-05-29 19:19:40 +0700

    Tamba selector utama

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function () {
+    const $body = $('body');
+    const $navbarContainer = $('.navbar .container');
+
+    const $projectRow = $('#proyek .row');
+    const $aboutContainer = $('#tentang .container');
+    const $blogContainer = $('#artikel .container');
+    const $contactForm = $('#kontak form');
+    const $achievementsSection = $('#prestasi');
+    const $heroContainer = $('#beranda .container');
